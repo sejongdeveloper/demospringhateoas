56d373f879a62a469ca6b3dab701d62752ac3adf
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-07 12:41:24 +0900

    Initial commit

---
+++ src/test/java/me/whiteship/demospringhateoas/DemospringhateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.demospringhateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringhateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
